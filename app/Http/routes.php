7353f0733da199618d0baf3a97207c284939fb2b
mkhsojib <mkhsojib@gmail.com>
2017-06-04 12:58:02 +0600

    Polymorphic relation many to many - retrieving

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -345,10 +345,22 @@ Route::get('/insert', function () {
 //});
 
 
-Route::get('photo/{id}/post', function ($id) {
+//Route::get('photo/{id}/post', function ($id) {
+//
+//    $photo = Photo::findOrFail($id);
+//
+//    return $photo->imageable;
+//
+//});
 
-    $photo = Photo::findOrFail($id);
+// Polymorphic relation many to many
 
-    return $photo->imageable;
+Route::get('post/tag', function () {
 
+    $post = Post::find(1);
+
+    foreach ($post->tags as $tag){
+
+        echo $tag->name;
+    }
 });
